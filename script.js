fb167b27231b680bc11d575f530ed33fb1680cfa
Pravesh288 <shresthapravesh288@gmail.com>
2024-06-11 00:03:36 +0545

    all done

---
+++ script.js
@@ -0,0 +1,18 @@
+const containerE1 = document.querySelector(".container");
+const carrers = ["YouTuber", "Web Developer", "Freelancer", "Instructor"];
+let carrerIndex= 0
+let characterIndex= 0
+
+updateText()
+function updateText(){
+containerE1.innerHTML= `<h1>I am ${carrers[carrerIndex].slice(0,1) === "I" ? "an" : "a"} ${carrers[carrerIndex].slice(0, characterIndex)}`;
+characterIndex++
+if(characterIndex===carrers[carrerIndex].length){
+    carrerIndex++
+    characterIndex=0;
+}
+if (carrerIndex===carrers.length){
+    carrerIndex=0;
+}
+setTimeout(updateText, 400);
+}
